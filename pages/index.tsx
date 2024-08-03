23a9e5a4bc9e05e527015798a37d1a879f37e94a
Eesa Zahed <eszhd1@gmail.com>
2024-08-03 19:34:18 -0400

    changed dark modde css

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,10 @@ const Home: NextPage = () => {
       <Head>
         <title>Upload an image</title>
       </Head>
-      <main className="pt-64 w-full min-h-screen leading-8 text-xl">
+      <main className="pt-32 w-full min-h-screen leading-8 text-xl">
+        <h1 className="text-7xl font-bold bg-gradient-to-r from-[#4b0bff] to-[#68b2ff] bg-clip-text text-transparent dark:from-[#4facfe] dark:to-[#00f2fe] text-center pb-8 mb-8">
+          1img
+        </h1>
         <ImageUploadForm />
       </main>
       <p className="my-16 text-center">
